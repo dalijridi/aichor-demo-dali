cc03d036a4fe19f5165d35311ac4dda67a4c5460
Khalil Dhouib <k.dhouib@instadeep.com>
2024-02-14 14:03:48 +0100

    EXP: first draft

---
+++ slurm-helloworld/src/deploy-with-conda.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --ntasks=1                # Number of tasks (see below)
+#SBATCH --cpus-per-task=1         # Number of CPU cores per task
+#SBATCH --nodes=1                 # Ensure that all cores are on one machine
+#SBATCH --time=0-00:05            # Runtime in D-HH:MM
+#SBATCH --partition=gpu-2080ti-dev # Partition to submit to
+#SBATCH --gres=gpu:1              # optionally type and number of gpus
+#SBATCH --mem=50G                 # Memory pool for all cores (see also --mem-per-cpu)
+#SBATCH --output=logs/job_%j.out  # File to which STDOUT will be written
+#SBATCH --error=logs/job_%j.err   # File to which STDERR will be written
+#SBATCH --mail-type=FAIL           # Type of email notification- BEGIN,END,FAIL,ALL
+#SBATCH --mail-user=<your-email>  # Email to which notifications will be sent
+
+# print info about current job
+# echo "---------- JOB INFOS ------------"
+#scontrol show job $SLURM_JOB_ID 
+# echo -e "---------------------------------\n"
+
+# Due to a potential bug, we need to manually load our bash configurations first
+source $HOME/.bashrc
+
+# install mini-conda
+#curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o ~/miniconda.sh -u
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p $HOME/miniconda
+source ~/miniconda/bin/activate
+conda init bash
+
+#create myenv
+source $HOME/.bashrc
+conda create -y -n myenv 
+pip install numpy torch ipdb
+
+# Next activate the conda environment 
+conda activate myenv
+
+# Run our code --use-gpu
+echo "-------- PYTHON OUTPUT ----------" 
+python3 multiply.py --timer_repetitions 10000 --no-gpu 
+echo "---------------------------------"
+
+# Deactivate environment again
+conda deactivate
---
+++ slurm-helloworld/src/multiply.py
@@ -0,0 +1,66 @@
+import torch
+from timeit import timeit
+import argparse
+
+def time_multiplication(
+    timer_repetitions,
+    use_gpu=True,
+    #data_path="/mnt/qb/baumgartner/storagetest.txt",
+    random_seed=0
+):
+
+    # Simulate external data access by reading the matrix size from external file
+    #with open(data_path) as f:
+    #    s = int(f.read())
+    s = 5
+
+    # Set random seed
+    torch.manual_seed(random_seed)
+
+    # Create tensors
+    x = torch.randn(32, s)
+    y = torch.randn(s, 32)
+    
+    # Check if Cuda available and move tensor to Cuda if yes
+    cuda_available = torch.cuda.is_available() 
+    print(f"Cuda_available={cuda_available}")
+    if cuda_available and use_gpu:
+        device = torch.cuda.current_device()
+        print(f"Current Cuda device: {device}")
+        x = x.to(device)
+        y = y.to(device)
+
+    # Multiply matrix first once for result and then multiple times for measuring elapsed time
+    mult = torch.matmul(x, y)
+    elapsed_time = timeit(lambda: torch.matmul(x, y), number=timer_repetitions)
+
+    # Print some results:
+    print("result:")
+    print(mult)
+    print("Output shape", mult.shape)
+    print(f"elapsed time: {elapsed_time}")
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Tescript for Singularity and SLURM on GPU. It times the multiplication of two matrices, repeated `timer_repetition` times."
+    )
+    parser.add_argument(
+        "--timer_repetitions", dest="timer_repetitions", action="store", default=1000, type=int, help="How many times to repeat the multiplication.",
+    )
+    parser.add_argument('--use-gpu', dest='use_gpu', action='store_true')
+    parser.add_argument('--no-gpu', dest='use_gpu', action='store_false')
+    parser.set_defaults(use_gpu=True)
+    parser.add_argument(
+        "--random-seed", dest="random_seed", action="store", default=0, type=int, help="Random seed for sampling random matrices.",
+    )
+    args = parser.parse_args()
+
+    # Run main function with command line input, to test singularity arguments
+    time_multiplication(
+        timer_repetitions=args.timer_repetitions, 
+        use_gpu=args.use_gpu, 
+        random_seed=args.random_seed,
+        )
+
+    print("done.")
